472cd732f162fbaf5246669e67eb2b1569720469
shinichi923 <shinichi923@gmail.com>
2014-01-02 22:15:42 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TenDaysDemo::Application.config.secret_key_base = '97e7dcb25567606cfa4efd0ac055a39dce5f6b0c81c614e2db805b5cf735eeabc9ca37d43cb051eb9e956b39509f0a57db687f35bb28f4aee0da183712cae253'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TenDaysDemo::Application.config.session_store :cookie_store, key: '_ten_days_demo_session'
